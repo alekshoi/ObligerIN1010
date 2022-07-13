3bc1639e47c73bc281f80fcbd9a686f9d207a346
Aleksander Høiness <93198257+alekshoi@users.noreply.github.com>
2022-07-13 12:51:29 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Controller kontrol = new Controller();
-        kontrol.hentModell().startSpill();
-        kontrol.klokke();
-    }
-}
